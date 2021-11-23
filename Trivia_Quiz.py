46a2e113a9dbd9c5f1400b64ba53a246b8037641
CharlieG12344 <charlie.gieger@vtsd.com>
2021-11-23 11:45:43 -0500

    Started working on methods and created question list

--- Trivia_Quiz.py
+++ Trivia_Quiz.py
@@ -5,13 +5,16 @@ Keep track of score
 Create leaderboard
 '''
 import turtle as trtl
+import random as rand
 wn = trtl.Screen()
 
 # Variables
 painter = trtl.Turtle()
+writer = trtl.Turtle()
 font = ("Arial", 50, "bold")
 font_two = ("Arial", 50, "normal")
-
+biology_list = ["What are the parts of a cell called?", "What gives blood it's red color?", "How many bones are in the body?"]
+music_list = ["What does a staccato do?", "How many beats does a whole note have?", "What is the sharp version of D flat?"]
 
 # Background Things
 wn.bgcolor("light blue")
@@ -28,17 +31,19 @@ def make_background(painter):
     painter.write("Press b to choose: Music", font=font_two)
 
 # Methods
-def category_A(painter):
+def category_Bio(biology):
+    painter.clear()
+    writer.write(biology, font=font_two)
+
+
+def category_Mus():
     painter.clear()
 
 
-def category_B(painter):
-    painter.clear()
-
-
+writer.hideturtle()
 make_background(painter)
-wn.onkeypress(category_A, "a")
-wn.onkeypress(category_B, "b")
+wn.onkeypress(category_Bio, "a")
+wn.onkeypress(category_Mus, "b")
 
 
 
