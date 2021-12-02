dd5db8152035b338cbf816ce49d486000bc7f11d
CharlieG12344 <charlie.gieger@vtsd.com>
2021-12-02 13:53:32 -0500

    Tried to fix error

--- Trivia_Quiz.py
+++ Trivia_Quiz.py
@@ -22,8 +22,8 @@ bio_length = len(biology_list)
 index = mus_length - 1
 index_B = bio_length - 1
 score = 0
-x = 0
-
+question_number = 0
+question_number_mus = 0
 
 # Background Things
 wn.bgcolor("light blue")
@@ -42,35 +42,107 @@ def make_background(painter):
 # Methods
 
 def category_Bio():
+    global question_number
     painter.clear()
-    x = 0
-    #while x < 3:
     writer.penup()
     question = biology_list.pop(index_B)
     writer.goto(-400, 200)
     writer.write(question, font=font_three)
     Bio_Questions()
+    question_number += 1
     wn.onkeypress(score_change, "b")
+    wn.onkeypress(bio_two, "c")
+    wn.onkeypress(bio_two, "a")
 
 def category_Mus():
+    global question_number_mus
+    mus_length = len(music_list)
+    index = mus_length - 1
     painter.clear()
-    x = 0
-    while x < 3:
-        writer.penup()
-        question_two = music_list.pop(index)
-        writer.goto(-400,200)
-        writer.write(question_two, font=font_three)
-        Mus_Questions()
-        wn.onkeypress(score_change, "c")
+    writer.penup()
+    question_two = music_list.pop(index)
+    writer.goto(-400,200)
+    writer.write(question_two, font=font_three)
+    Mus_Questions()
+    question_number_mus += 1
+    wn.onkeypress(score_change, "c")
+    wn.onkeypress(mus_two, "b")
+    wn.onkeypress(mus_two, "a")
+
+def bio_two():
+    question_number = 1
+    bio_length = len(biology_list)
+    index_B = bio_length - 1
+    writer.clear()
+    question = biology_list.pop(index_B)
+    writer.goto(-400, 200)
+    writer.write(question, font=font_three)
+    Bio_Questions()
+    question_number += 1
+    wn.onkeypress(score_change, "c")
+    wn.onkeypress(bio_three, "b")
+    wn.onkeypress(bio_three, "a")
+
+
+def mus_two():
+    question_number_mus = 1
+    mus_length = len(music_list)
+    index_A = mus_length - 1
+    writer.clear()
+    question_two = music_list.pop(index_A)
+    writer.goto(-400, 200)
+    writer.write(question_two, font=font_three)
+    Mus_Questions()
+    question_number_mus += 1
+    wn.onkeypress(score_change, "b")
+    wn.onkeypress(mus_three, "c")
+    wn.onkeypress(mus_three, "a")
+
+def mus_three():
+    question_number_mus = 2
+    writer.clear()
+    writer.goto(-400, 200)
+    writer.write("What does a staccato mean?", font=font_three)
+    Mus_Questions()
+    question_number_mus += 1
+    wn.onkeypress(score_change, "a")
+    wn.onkeypress(end, "c")
+    wn.onkeypress(end, "b")
+
+def bio_three():
+    question_number = 2
+    writer.clear()
+    writer.goto(-400, 200)
+    writer.write("What are the parts of a cell called?", font=font_three)
+    Bio_Questions()
+    question_number += 1
+    wn.onkeypress(score_change, "a")
+    wn.onkeypress(end, "c")
+    wn.onkeypress(end, "b")
 
 def score_change():
-    global score, x
+    global score
     scorekeeper.penup()
-    scorekeeper.goto(225,250)
+    scorekeeper.goto(250,250)
     score += 1
     scorekeeper.clear()
     scorekeeper.write(score, font=font)
-    x += 1
+    # No error with score
+    if question_number == 1:
+        bio_two()
+    elif question_number_mus == 1:
+        mus_two()
+    elif question_number == 2:
+        bio_three()
+    elif question_number_mus == 2:
+        mus_three()
+    elif question_number == 3:
+        end()
+    elif question_number_mus == 3:
+        end()
+    else:
+        end()
+
 
 def Mus_Questions():
     writer.goto(-375,50)
@@ -91,13 +163,18 @@ def Bio_Questions():
 def end():
     global score, font
     writer.clear()
-    painter.goto(-300, 200)
+    scorekeeper.clear()
+    scorekeeper.penup()
+    writer.penup()
+    scorekeeper.goto(-50, 0)
+    scorekeeper.write(score, font=font)
+    painter.goto(-275, 200)
     if score == 3:
-        painter.write("Congrats! You got every question correct.", font=font)
+        painter.write("Congrats! You got every question correct.", font=font_three)
     elif score >= 1:
-        painter.write("Congrats! You got some of the questions correct", font=font)
+        painter.write("Congrats! You got some of the questions correct.", font=font_three)
     else:
-        painter.write("You got none od the questions rights.", font=font)
+        painter.write("You got none of the questions rights!", font=font_three)
 
 
 scorekeeper.hideturtle()
@@ -105,8 +182,6 @@ writer.hideturtle()
 make_background(painter)
 wn.onkeypress(category_Bio, "a")
 wn.onkeypress(category_Mus, "b")
-end()
-
 
 
 wn.listen()
